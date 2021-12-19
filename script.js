c1a9416e66c7b8291cb429a515a81965ea5dbb2a
Ayman Choughari <ayman.choughari@gmail.com>
2021-12-19 21:23:22 +0100

    Modify buttons and color choice

--- script.js
+++ script.js
@@ -32,19 +32,15 @@ function hoverDisplay(color='black'){
     const squares=document.querySelectorAll(".square");
     squares.forEach(gridSquare => {
         gridSquare.addEventListener("mouseover" , function(evt) {
-            if (color==="Rainbow") {
+            if (color==="rainbow") {
                 evt.target.style.background=randomHexColor();
-                console.log("rainbow fail")
             }
             else {
             evt.target.style.background=color;
-            console.log(`this is ${color}`)
             }
         })
     })
 }
-
-
 function onClickReset(){
 const btn=document.querySelector(".btnClear")
      btn.addEventListener("click", deleteGrid)
@@ -54,34 +50,26 @@ const btn=document.querySelector(".btnClear")
      btn.addEventListener("click", function() {toggleContainerGrid(true)})
      btn.addEventListener("click", function() {hoverDisplay('black')})
     }
-
 function chooseColor(){
     const btns=document.querySelectorAll(".btnColor")
     btns.forEach(btn => {
-        btn.addEventListener("click", function() {hoverDisplay(this.textContent)})
+        btn.addEventListener("click", function() {hoverDisplay(this.id)})
         })
     }
-
 function Eraser(){
     const btn=document.querySelector(".btnEraser")
     btn.addEventListener("click", function() {hoverDisplay("white")})  
 }
-
 function deleteGrid() {
     const squares=document.querySelectorAll(".square");
     squares.forEach(square=>square.remove())
 }
-
 function chooseGridSize() {
     rowLenght=window.prompt("Please pick a grid row lenght")
     gridSize=rowLenght**2
     squareSize=`${100/rowLenght}%`
 }
-
-
 function randomHexColor(){
     const randomHexColor='#000000'
-    //const hexConversion={0:0,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:"a",11:"b",12:"c",13:"d",14:"e",15:"f"}
-    //return randomHexColor.replace(/0/g,  function(){return hexConversion[Math.floor(Math.random()*16)]})
     return randomHexColor.replace(/0/g,  function(){return Math.floor(Math.random()*16).toString(16)})
 }
